4e88954b324697245435e30255c45009c4ab20d7
Dani <danisuesmith@gmail.com>
2022-06-11 13:14:38 -0700

    commit tree df8ceda5eead169d5111df348b8ce6f224d8da74

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name,
+    email: email,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((response) => {
+      document.body.innerHTML = `<p>${response.id}</p>`;
+    })
+    .catch((error) => {
+      document.body.innerHTML = `<p>${error.message}</p>`;
+    });
+}
